d4667fd15083d0699b39397dd5935feba54dfe40
allanalvss <45926898+allanalvss@users.noreply.github.com>
2019-02-21 10:09:54 -0300

    Update conta_99.vhd

--- conta_99.vhd
+++ conta_99.vhd
@@ -3,7 +3,7 @@
 -- Entradas: clk (clock), sobedesce e reset - Tipo bit
 -- Saidas: HEX0 e HEX1 (displays 7 seg) - Tipo vetor 7 posicoes
 -- Autores: Joao Vitor e Marcos Meira
--- Modificado por: Andressa Theotõnio e Calebe Oliveira
+-- Modificado por: ALLAN VICTOR ALVES DE LIMA
 -- Data: 2/8/2017
  
 library IEEE;                                                        
